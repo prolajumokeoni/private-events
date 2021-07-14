339ca43095f06eab7d66d1d1c7466da0dd9557e5
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-14 21:11:05 +0100

    Add link to user's showpage and restrict user to one event

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,9 +1,14 @@
 class RegistersController < ApplicationController
   before_action :authenticate_user!
   def create
-    @event = Event.find(params[:event_id])
-    @register = @event.registers.create(register_params.merge(user_id: current_user.id))
-    redirect_to event_path(@event)
+    event = Event.find(params[:event_id])
+    @register = Register.new(event_id: event.id, user_id: current_user.id)
+
+    if Register.where(user_id: current_user.id, event_id: params[:event_id]).exists?
+      redirect_to event_path(event)
+      return
+    end
+
   end
 
   private
